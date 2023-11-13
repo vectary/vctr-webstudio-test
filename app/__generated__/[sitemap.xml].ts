a55248de0af2a950f40028ac65fdbafed65451f6
Alexander Mescheryakov <sibsfinx@ya.ru>
2023-11-13 14:08:08 +0100

    init + sync

---
+++ app/__generated__/[sitemap.xml].ts
@@ -0,0 +1,14 @@
+
+      export const sitemap = {
+  "pages": [
+    {
+      "path": "",
+      "lastModified": "2023-11-13T13:00:16.623Z"
+    },
+    {
+      "path": "/ai-test",
+      "lastModified": "2023-11-13T13:00:16.623Z"
+    }
+  ]
+};
+    
---
+++ app/__generated__/_index.tsx
@@ -0,0 +1,162 @@
+/* eslint-disable */
+/* This is a auto generated file for building the project */ 
+
+import { type ReactNode, useState } from "react";
+import type { PageData } from "~/routes/_index";
+import type { Asset, ImageAsset, SiteMeta } from "@webstudio-is/sdk";
+import { Body as Body, Box as Box, Image as Image, Heading as Heading, Text as Text } from "@webstudio-is/sdk-components-react";
+
+export const fontAssets: Asset[] = []
+export const imageAssets: ImageAsset[] = [{"id":"09a04c1e-28d3-4a28-ae61-6f1079a9c823","name":"card-get-started-icon-ar_1grSx8GEKfADhVrfxzQrS.svg","description":null,"projectId":"b35a1473-4ef1-4be3-93d2-30cf8d9ebf0f","size":2641,"type":"image","format":"svg","createdAt":"2023-11-13T12:40:51.697Z","meta":{"width":25,"height":28}}]
+export const pageData: PageData = {"page":{"id":"NbEnUgcJPvVbhnkMtRFnG","name":"Home","title":"Home","meta":{},"rootInstanceId":"VUSkho34c4ybN1utYmVO8","path":""}};
+export const user: { email: string | null } | undefined = {"email":"meshcheriakov@vectary.com"};
+export const projectId = "b35a1473-4ef1-4be3-93d2-30cf8d9ebf0f";
+
+const Page = (props: { scripts?: ReactNode }) => {
+return <Body
+data-ws-id="VUSkho34c4ybN1utYmVO8"
+data-ws-component="Body">
+<Box
+data-ws-id="bsfj-Ov9S4dgAo-AB2EFF"
+data-ws-component="Box">
+<Box
+data-ws-id="HY-Q9CE2YzismzGfMsPS6"
+data-ws-component="Box">
+<Box
+data-ws-id="WPPbAudve_CpuGfOd3Cwu"
+data-ws-component="Box">
+<Box
+data-ws-id="QItzzEa6MPyeCOmP-aWsh"
+data-ws-component="Box">
+<Image
+data-ws-id="CMWzgTUCd5wu5cYgw9Qi5"
+data-ws-component="Image"
+src={"/assets/card-get-started-icon-ar_1grSx8GEKfADhVrfxzQrS.svg"}
+width={25}
+height={28} />
+</Box>
+<Box
+data-ws-id="RbiJ9LGkuTdNxcu_lV_7U"
+data-ws-component="Box">
+<Heading
+data-ws-id="ozKmRy31ahKcmXaP4RkZO"
+data-ws-component="Heading">
+{"Augmented Reality"}
+</Heading>
+<Text
+data-ws-id="iBmoU8Th7AZbOYQrVVif8"
+data-ws-component="Text">
+{"Place designs in real environments"}
+</Text>
+</Box>
+</Box>
+</Box>
+<Box
+data-ws-id="5gsigEcOY_-7EEbTGL_ZU"
+data-ws-component="Box">
+<Box
+data-ws-id="0St7Lu8SbMVg0DMtBuhPs"
+data-ws-component="Box">
+<Box
+data-ws-id="hT-9MvovEmwGlGzDPzDxb"
+data-ws-component="Box">
+<Image
+data-ws-id="4VCXnAS8QQAwa2_dHA1P7"
+data-ws-component="Image"
+src={"/assets/card-get-started-icon-ar_1grSx8GEKfADhVrfxzQrS.svg"}
+width={25}
+height={28} />
+</Box>
+<Box
+data-ws-id="QIt8vvv-ensXCZqpcWXA3"
+data-ws-component="Box">
+<Heading
+data-ws-id="ao6HOCnZdcs2Jczn291NV"
+data-ws-component="Heading">
+{"Augmented Reality"}
+</Heading>
+<Text
+data-ws-id="NuLLWk7Q9yjN435MsIlhJ"
+data-ws-component="Text">
+{"Place designs in real environments"}
+</Text>
+</Box>
+</Box>
+</Box>
+<Box
+data-ws-id="KvVw4ZSqSUJPnYKENYZdD"
+data-ws-component="Box">
+<Box
+data-ws-id="stPdIrK2BhoE6v4fZIuzq"
+data-ws-component="Box">
+<Box
+data-ws-id="ygdMET686TTz2yUVFAvEk"
+data-ws-component="Box">
+<Image
+data-ws-id="fbbIWe1bmPRbJuWEwHbZJ"
+data-ws-component="Image"
+src={"/assets/card-get-started-icon-ar_1grSx8GEKfADhVrfxzQrS.svg"}
+width={25}
+height={28} />
+</Box>
+<Box
+data-ws-id="CsdIbc9NP6ELij-KrbMtS"
+data-ws-component="Box">
+<Heading
+data-ws-id="erFo8COZ_glKwN073AFIC"
+data-ws-component="Heading">
+{"Augmented Reality"}
+</Heading>
+<Text
+data-ws-id="CRSLmuT__-u8onSgNlJUZ"
+data-ws-component="Text">
+{"Place designs in real environments"}
+</Text>
+</Box>
+</Box>
+</Box>
+<Box
+data-ws-id="to0zMflfOQqflszBdiqc9"
+data-ws-component="Box">
+<Box
+data-ws-id="ggWwgICJ97fJ7uKDGqpuS"
+data-ws-component="Box">
+<Box
+data-ws-id="FEXJvFQ5Z8Wj5MpLkZaZ1"
+data-ws-component="Box">
+<Image
+data-ws-id="7gdBwu3yKuUZ2nm42pGDn"
+data-ws-component="Image"
+src={"/assets/card-get-started-icon-ar_1grSx8GEKfADhVrfxzQrS.svg"}
+width={25}
+height={28} />
+</Box>
+<Box
+data-ws-id="qtbrH00jcgL2cPRxKPo06"
+data-ws-component="Box">
+<Heading
+data-ws-id="FlxYh3mjAzo5zwK64FYbQ"
+data-ws-component="Heading">
+{"Augmented Reality"}
+</Heading>
+<Text
+data-ws-id="GeFhq0AURqdhKCNNlreby"
+data-ws-component="Text">
+{"Place designs in real environments"}
+</Text>
+</Box>
+</Box>
+</Box>
+</Box>
+{props.scripts}
+</Body>
+}
+
+
+export { Page }
+
+
+  export const pagesPaths = new Set(["","/ai-test"])
+
+  export const formsProperties = new Map<string, { method?: string, action?: string }>([])
+  
