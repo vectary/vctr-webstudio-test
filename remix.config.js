eb8a250dfb1f6098ec80bdc2e04572d1ef714d54
Alexander Mescheryakov <sibsfinx@ya.ru>
2023-11-14 14:10:05 +0100

    re-init webstudio with netlify edge functions

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,23 @@
+const { config } = require("@netlify/remix-edge-adapter");
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? config
+    : {
+        ignoredRouteFiles: ["**/.*"],
+        serverModuleFormat: "cjs",
+        serverDependenciesToBundle: [
+          /@webstudio-is\//,
+          "nanoid",
+          "@jsep-plugin/assignment",
+          "change-case",
+          "title-case",
+        ],
+      };
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  serverModuleFormat: "cjs",
-  serverDependenciesToBundle: [
-    /@webstudio-is\//,
-    "nanoid",
-    "@jsep-plugin/assignment",
-    "change-case",
-    "title-case",
-  ],
+  ...baseConfig,
   future: {
     v2_errorBoundary: true,
     v2_headers: true,
